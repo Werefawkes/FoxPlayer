84e0f19773a9b52a7c45ee2752182be0cd89a6d7
Werefox <jack@wrightus.com>
2023-11-13 15:33:55 -0500

    Stubbed a player script

---
+++ Runtime/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Foxthorne.FoxInventory;
+
+namespace Foxthorne.PlayerController
+{
+	public class Player : MonoBehaviour
+	{
+		[Header("Movement")]
+		Vector2 moveInput;
+		[Header("Interaction")]
+		public SO_ItemData selectedItemData;
+
+		void Start()
+		{
+
+		}
+
+		void Update()
+		{
+
+		}
+	}
+}
