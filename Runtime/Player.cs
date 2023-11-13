3e33d5bdc2c0dbc95f4d4aa8ae02e53cb79fef28
Werefox <jack@wrightus.com>
2023-11-13 16:14:16 -0500

    Added some code to handle movement and camera looking

--- Runtime/Player.cs
+++ Runtime/Player.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Foxthorne.FoxInventory;
+using UnityEngine.InputSystem;
 
 namespace Foxthorne.PlayerController
 {
 	public class Player : MonoBehaviour
 	{
 		[Header("Movement")]
-		Vector2 moveInput;
+		public float moveSpeed = 1;
+		public Vector2 moveInput;
+
+		public float lookSpeed = 1;
+		public float minLookAngle = -80;
+		public float maxLookAngle = 80;
+		Vector2 lookInput;
+		public Camera playerCam;
+
 		[Header("Interaction")]
 		public SO_ItemData selectedItemData;
+
+		[Header("Input")]
+		PlayerInput PlayerInput;
+
+		[Header("References")]
+		public Rigidbody rb;
 
 		void Start()
 		{
@@ -19,7 +34,45 @@ namespace Foxthorne.PlayerController
 
 		void Update()
 		{
-
+			DoMovement();
+			DoLook();
 		}
+
+		#region Movement
+		void DoMovement()
+		{
+			Vector3 velocity = Vector3.zero;
+			velocity.x = moveInput.x * moveSpeed;
+			velocity.z = moveInput.y * moveSpeed;
+			rb.velocity = velocity;
+		}
+
+		void DoLook()
+		{
+			Vector3 angles = playerCam.transform.localEulerAngles;
+			angles.x += -lookInput.y * lookSpeed;
+			if (angles.x > 270)
+			{
+				angles.x -= 360;
+			}
+			angles.x = Mathf.Clamp(angles.x, minLookAngle, maxLookAngle);
+
+			angles.y += lookInput.x * lookSpeed;
+			playerCam.transform.localEulerAngles = angles;
+		}
+
+		// Input methods
+		void OnMove(InputValue value)
+		{
+			moveInput = value.Get<Vector2>();
+		}
+
+		void OnLook(InputValue value)
+		{
+			lookInput = value.Get<Vector2>();
+		}
+		#endregion Movement
+
+
 	}
 }
